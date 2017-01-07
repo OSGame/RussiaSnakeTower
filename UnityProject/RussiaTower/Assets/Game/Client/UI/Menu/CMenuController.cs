bee87e10792c20db6b2b249dcb005b5e2d3868b2
dczwx5 <dczwx5@126.com>
2017-01-07 23:52:12 +0800

    add menu

---
+++ UnityProject/RussiaTower/Assets/Game/Client/UI/Menu/CMenuController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Start");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnStart() {
+        print("onStart");
+    }
+}
