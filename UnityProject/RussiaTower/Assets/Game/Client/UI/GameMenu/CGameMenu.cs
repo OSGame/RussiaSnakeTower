6564b46345bfb24227e64cebe7ecbfad4ee8d748
dczwx5 <dczwx5@126.com>
2017-01-08 01:10:34 +0800

    add exitBtn in tower, can exit to menu and enter gameStage now

---
+++ UnityProject/RussiaTower/Assets/Game/Client/UI/GameMenu/CGameMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CGameMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnExit() {
+        SceneManager.LoadScene("Menu");
+    }
+}
